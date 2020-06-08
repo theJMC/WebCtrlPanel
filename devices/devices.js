cbbe03fa76dbb45d5177cbaedc9f724adcb1f643
James (WSL) <jamesmccarthy150@gmail.com>
2020-06-08 12:20:01 +0100

    added functionallity to devices.js

--- devices/devices.js
+++ devices/devices.js
@@ -1,2 +1,17 @@
 // Parse the devices.json file to add devices to the device list
 
+var xmlhttp = new XMLHttpRequest();
+var url = "devices.json";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        myFunction(myArr);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    alert(arr);
+}
